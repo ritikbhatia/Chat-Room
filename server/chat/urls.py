836353ed47a112bf1a07c094eb4939dadcbe3fb2
ritikbhatia <bhatia.ritik5@gmail.com>
2020-05-24 00:26:46 +0800

    Chat room functionalities

---
+++ server/chat/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+import django_eventstream
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('<room_id>', views.home),
+    path('rooms/<room_id>/messages/', views.messages),
+    path('rooms/<room_id>/events/', include(django_eventstream.urls),
+         {'format-channels': ['room-{room_id}']}),
+
+    # older endpoint allowing client to select channel. not recommended
+    path('/events/', include(django_eventstream.urls)),
+]
